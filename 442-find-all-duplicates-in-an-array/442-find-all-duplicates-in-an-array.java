228b46f1feea6d72d139e399e94ac9ba8a8a454d
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-06-18 01:05:44 +0530

    Time: 10 ms (48.96%), Space: 65.5 MB (69.73%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
@@ -3,12 +3,15 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         
         for(int i=0; i<nums.length; i++){
+// Only take positive Integers
             int n = Math.abs(nums[i]);
             
+// If num[1] is already negative then then add n i.e 2 to the list as duplicate
             if(nums[n-1] < 0)
                 list.add(Math.abs(n));
-            
-            nums[n-1] = -nums[n-1];
+        
+// Mark n-1th idx as -ve
+            nums[n-1] = -nums[n-1]; // if n=2 do nums[1] = -nums[1] to mark that 2 has been visited 
         }
         
         return list;
