3a91d6b81b3e6d86c32e072c8b6d3c51d9eef361
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-06-17 19:09:51 +0530

    Added solution - LeetHub

---
+++ Smallest Positive Integer that can not be represented as Sum - GFG/smallest-positive-integer-that-can-not-be-represented-as-sum.java
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+            int n = sc.nextInt();
+            long array[] = new long[n];
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = sc.nextLong();
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.smallestpositive(array,n));
+            t--;
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution { 
+    long smallestpositive(long[] array, int n){ 
+        // Your code goes here 
+        Arrays.sort(array);
+        long sumIntegerNot = 1, sumTillNow = 0;
+        for(int i = 0; i < n; i++){
+            if(array[i] <= sumIntegerNot){
+                sumTillNow += array[i];
+                sumIntegerNot = sumTillNow + 1;
+            }
+            else{
+                return sumIntegerNot;
+            }
+        }
+        return sumIntegerNot;
+    }
+} 
