366531719b4a6e708dc2b987f9d5eb0891b7ef75
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-06-19 01:41:49 +0530

    Time: 65 ms (31.44%), Space: 60.3 MB (29.11%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,28 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        
+        int n = nums.length;
         Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
         List<List<Integer>> res = new ArrayList<>();
-        Set<List<Integer>> set = new HashSet<>();
-        for(int i=0; i<nums.length-2; i++){
-            
-            int l = i+1;
-            int h = nums.length-1;
-            int sum = 0-nums[i];
-            
-            while(l < h){
-                if(nums[l]+nums[h] == sum){
-                    List<Integer> n = Arrays.asList(nums[i],nums[l], nums[h]);
-                    Collections.sort(n);
-                    if(set.contains(n)){
-                        l++;
-                        h--;
-                        continue;
+        // Collections.sort(new ArrayList());
+        for(int i=0; i<n-2; i++){
+            if(i==0 || (nums[i-1] != nums[i])){
+                
+                int sum = 0-nums[i];
+                int lo = i+1;
+                int hi = n-1;
+                System.out.println(sum);
+                while(lo < hi){
+                    if(lo != i+1 && hi != n-1){
+                        while(hi > 1 && nums[hi] == nums[hi+1]) hi--;
+                        while(lo < n-1 && nums[lo] == nums[lo-1]) lo++;
                     }
-                    res.add(n);
-                    set.add(n);
+                    if(lo >= hi) continue;
+                    if(nums[lo]+nums[hi] == sum){
+                        res.add(Arrays.asList(nums[hi], nums[lo], -sum));
+                        lo++;
+                        hi--;
+                    }
+                    else if(nums[lo]+nums[hi] < sum){
+                        lo++;
+                    }
+                    else
+                        hi--;
                 }
-                else if (nums[l] + nums[h] < sum) l++;
-                else h--;
             }
         }
         
