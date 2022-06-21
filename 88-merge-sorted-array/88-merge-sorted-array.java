ac29149cab0fd7efadec0b2fbb481bb48a759579
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-06-22 00:58:08 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (47.38%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,14 +1,13 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(n == 0) return;
+        // if(n == 0) return;
         
         
         int i = m-1;
         int j = n-1;
         int k = nums1.length-1;
         
-        while(i >= 0 && j>=0){
-                    // System.out.println("In Upper Loop");
+        while(i >= 0 && j >= 0){
             if(nums1[i] > nums2[j]){
                 nums1[k--] = nums1[i--];
             }else{
@@ -17,7 +16,6 @@ class Solution {
         }
         
         while(j >= 0){
-            // System.out.println("In Lower Loop");
             nums1[k--] = nums2[j--];
         }
         
