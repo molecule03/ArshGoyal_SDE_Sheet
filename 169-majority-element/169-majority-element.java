8e1d2fa9f5e800d8e64f2a002d41360c48520567
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-06-22 01:26:36 +0530

    Time: 2 ms (86.12%), Space: 56 MB (50.79%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+        
+        int count = 0;
+        int element = 0;
+        
+        for(int i : nums){
+            if(count == 0){
+                element = i;
+            }
+            if(element == i){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        
+        return element;
+    }
+}
