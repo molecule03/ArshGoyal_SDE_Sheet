24be18fe52063c75e206ae7db35701eda4bdd664
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-06-24 23:50:34 +0530

    Added solution - LeetHub

---
+++ Finding Profession - GFG/finding-profession.java
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String a[] = in.readLine().trim().split("\\s+");
+            int level = Integer.parseInt(a[0]);
+            int pos = Integer.parseInt(a[1]);
+            
+            Solution ob = new Solution();
+            if(ob.profession(level, pos) == 'd')
+                System.out.println("Doctor");
+            else
+                System.out.println("Engineer");
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static char profession(int level, int pos){
+        // code here
+      // base case
+        if( pos == 1){
+            return 'e';
+        }
+        char parent = profession(level-1, (pos+1)/2);
+        
+        if(pos%2 !=0){
+            return parent;
+        }
+            return parent == 'e'? 'd':'e';
+    }
+}
