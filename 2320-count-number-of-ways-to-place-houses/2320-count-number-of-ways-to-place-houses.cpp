f34a545d0927ecf92d06a9d77a5d05d9a106ada6
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-01 20:57:20 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (69.65%) - LeetHub

---
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countHousePlacements(int n) {
+        
+               int mod = 1000000007;
+        
+        long val[n+5];// = new long[n+5];
+        
+        val[1] = 2;
+        val[2] = 3;
+        for(int i=3; i<=n; i++){
+            val[i] = (val[i-1]+val[i-2])%mod;
+        }
+        
+        long ans = (val[n]*val[n])%mod;
+        
+        return (int)ans;
+    }
+};
