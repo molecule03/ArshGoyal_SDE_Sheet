51bd399c17c1c6635a2e1dc71bc3a7a4ede5e5bb
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-04 00:57:53 +0530

    Time: 2 ms (100.00%), Space: 6.8 MB (66.67%) - LeetHub

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        
+        int mod = 1000000000+7;
+        
+        vector<long> dp(n+1);
+        dp[1] = 1;
+        
+        long noOfPeopleSharingSec = 0;
+        long ans = 0;
+        
+        for(int i=2; i<=n; i++){
+            long noOfNewPeopleSharingSec = dp[max(i-delay, 0)];
+            long noOfPeopleForgeting = dp[max(i-forget, 0)];
+            
+            noOfPeopleSharingSec += (noOfNewPeopleSharingSec-noOfPeopleForgeting+mod)%mod;
+            
+            dp[i] = noOfPeopleSharingSec;
+        }
+        
+        for(int i=n-forget+1; i<=n; i++){
+            ans = (ans+dp[i])%mod;
+        }
+        
+        return (int)ans;
+    }
+};
