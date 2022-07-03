6f3e76629189a58e62d578346727b6a625637396
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-03 10:05:39 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (53.75%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -4,7 +4,7 @@ class Solution {
         int r = mat.length;
         int c = mat[0].length;
         
-        boolean[][] isVis = new boolean[r][c];
+        // boolean[][] isVis = new boolean[r][c];
         List<Integer> list = new ArrayList<>();
         
         int colB = 0;
@@ -13,35 +13,41 @@ class Solution {
         int rowE = r-1;
         while(colB<=colE && rowB<=rowE){
             for(int i=colB; i<=colE; i++){
-                if(!isVis[rowB][i]){
+                // if(!isVis[rowB][i]){
                     list.add(mat[rowB][i]);
-                    isVis[rowB][i] = true;
-                }
+                    // isVis[rowB][i] = true;
+                // }
             }
             rowB++;
             
             for(int i=rowB; i<=rowE; i++){
-                 if(!isVis[i][colE]){
+                 // if(!isVis[i][colE]){
                     list.add(mat[i][colE]);
-                    isVis[i][colE] = true;
-                }
+                    // isVis[i][colE] = true;
+                // }
             }
             colE--;
 
+            if(rowB <= rowE){
             for(int i=colE; i>=colB; i--){
-                 if(!isVis[rowE][i]){
+                 // if(!isVis[rowE][i]){
                     list.add(mat[rowE][i]);
-                    isVis[rowE][i] = true;
-                }
+                    // isVis[rowE][i] = true;
+                // }
+            }
             }
             rowE--;
+            
+            if(colB <= colE){
             for(int i=rowE; i>=rowB; i--){
-                 if(!isVis[i][colB]){
+                 // if(!isVis[i][colB]){
                     list.add(mat[i][colB]);
-                    isVis[i][colB] = true;
-                }
+                    // isVis[i][colB] = true;
+                // }
+            }
             }
             colB++;
+            
         }
         return list;
     }
