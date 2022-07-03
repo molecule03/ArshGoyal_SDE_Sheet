eaf5492d9f5e47555a249c911a50ecd47b4bd7db
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-03 17:55:16 +0530

    Time: 298 ms (66.67%), Space: 128 MB (33.33%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -1,19 +1,21 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * struct ListNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
 class Solution {
-    public int[][] spiralMatrix(int m, int n, ListNode head) {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
         
-        int mat[][] = new int[m][n];
+        // int mat[][] = new int[m][n];
+        vector<vector<int>> mat(m, vector<int>(n, -1));
         
-        ListNode cur = head;
+        ListNode* cur = head;
         int colB = 0;
         int colE = n-1;
         int rowB = 0;
@@ -22,33 +24,33 @@ class Solution {
         while(colB <= colE && rowB <= rowE){
             
             for(int i=colB; i<=colE; i++){
-                if(cur == null){
+                if(cur == NULL){
                     mat[rowB][i] = -1;
                 }else{
-                    mat[rowB][i] = cur.val;
-                    cur = cur.next;
+                    mat[rowB][i] = cur->val;
+                    cur = cur->next;
                 }
             }
             rowB++;
             
             
             for(int i=rowB; i<=rowE; i++){
-                if(cur == null){
+                if(cur == NULL){
                     mat[i][colE] = -1;
                 }else{
-                    mat[i][colE] = cur.val;
-                    cur = cur.next;
+                    mat[i][colE] = cur->val;
+                    cur = cur->next;
                 }
             }
             colE--;
             
             if(rowB <= rowE){
             for(int i=colE; i>=colB; i--){
-                if(cur == null){
+                if(cur == NULL){
                     mat[rowE][i] = -1;
                 }else{
-                    mat[rowE][i] = cur.val;
-                    cur = cur.next;
+                    mat[rowE][i] = cur->val;
+                    cur = cur->next;
                 }
             }}
             rowE--;
@@ -56,11 +58,11 @@ class Solution {
             
             if(colB <= colE){
             for(int i=rowE; i>=rowB; i--){
-                if(cur == null){
+                if(cur == NULL){
                     mat[i][colB] = -1;
                 }else{
-                    mat[i][colB] = cur.val;
-                    cur = cur.next;
+                    mat[i][colB] = cur->val;
+                    cur = cur->next;
                 }
             }}
             colB++;
@@ -70,4 +72,4 @@ class Solution {
         
         return mat;
     }
-}
+};
