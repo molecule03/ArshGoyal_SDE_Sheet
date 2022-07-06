406a086fa2b0ee68b046570619ec9dc929b94400
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-06 14:03:10 +0530

    Time: 5 ms (45.27%), Space: 5.9 MB (80.05%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    int dp[31];
+public:
+    int fib(int n) {
+        if(n <= 1) return n;
+    
+        if(dp[n] != 0){
+            return dp[n];
+        }
+        int sum = fib(n-1)+fib(n-2);
+        dp[n] = sum;
+        
+        return sum;
+    }
+};
