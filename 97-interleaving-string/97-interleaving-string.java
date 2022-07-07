37ad3f3a9393d422dc65180431519cee70c810ac
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-07 20:43:15 +0530

    Time: 262 ms (5.16%), Space: 50.5 MB (7.37%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.java
@@ -0,0 +1,59 @@
+class Solution {
+    Map<String, Boolean> map = new HashMap<>();
+    int i = 0;
+    
+    public boolean isInterleave(String s1, String s2, String s3) {
+        
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int len3 = s3.length();
+        
+        if(len3 != len1+len2){
+            return false;
+        }
+        
+        char c1[] = s1.toCharArray();
+        char c2[] = s2.toCharArray();
+        char c3[] = s3.toCharArray();
+        
+        return isValid(c1, c2, c3, len1, len2, len3, 0, 0, 0);
+    }
+    
+    private boolean isValid(char[]c1, char[]c2, char[]c3, int len1, int len2, int len3, int p1, int p2, int p3){
+        
+        System.out.println("In isValis"+" "+i++);
+        if(p3 == len3){
+            return (p2==len2 && p1==len1);
+        }
+        
+        StringBuilder sb = new StringBuilder(""+p1+'*'+""+p2+'*'+""+p3);
+        String key = sb.toString();
+        System.out.println(key);
+        
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
+        
+        if(p1 == len1){
+            boolean flag = c2[p2]==c3[p3] ? isValid(c1, c2, c3, len1, len2, len3, p1, p2+1, p3+1) : false;
+            map.put(key, flag);
+            return map.get(key);
+        }
+        if(p2 == len2){
+            boolean f = c1[p1]==c3[p3] ? isValid(c1, c2, c3, len1, len2, len3, p1+1, p2, p3+1) : false;
+            map.put(key, f);
+            return map.get(key);
+        }
+        
+        boolean one = false, two = false;
+        if(c2[p2]==c3[p3]){
+            one = isValid(c1, c2, c3, len1, len2, len3, p1, p2+1, p3+1);
+        }
+        if(c1[p1]==c3[p3]){
+            two = isValid(c1, c2, c3, len1, len2, len3, p1+1, p2, p3+1);
+        }
+        
+        map.put(key, (one || two) );
+        return map.get(key);
+    }
+}
