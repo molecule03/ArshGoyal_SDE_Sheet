c89bbf78df1bab0b4d1887d3538931ac24a13851
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-08 12:52:15 +0530

    Time: 78 ms (26.88%), Space: 72 MB (19.38%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.java
+++ 1473-paint-house-iii/1473-paint-house-iii.java
@@ -1,51 +1,43 @@
 class Solution {
-    // Assign the size as per maximum value for different params
-    Integer[][][] memo = new Integer[100][100][21];
-    // Maximum cost possible plus 1
-    final int MAX_COST = 1000001;
-    
-    int findMinCost(int[] houses, int[][] cost, int targetCount, int currIndex,
-                    int neighborhoodCount, int prevHouseColor) {
-        if (currIndex == houses.length) {
-            // If all houses are traversed, check if the neighbor count is as expected or not
-            return neighborhoodCount == targetCount ? 0 : MAX_COST;
-        }
-        
-        if (neighborhoodCount > targetCount) {
-            // If the neighborhoods are more than the threshold, we can't have target neighborhoods
-            return MAX_COST;
-        }
-        
-        // We have already calculated the answer so no need to go into recursion
-        if (memo[currIndex][neighborhoodCount][prevHouseColor] != null) {
-            return memo[currIndex][neighborhoodCount][prevHouseColor];
-        }
-        
-        int minCost = MAX_COST;
-        // If the house is already painted, update the values accordingly
-        if (houses[currIndex] != 0) {
-            int newNeighborhoodCount = neighborhoodCount + (houses[currIndex] != prevHouseColor ? 1 : 0);
-            minCost = 
-                findMinCost(houses, cost, targetCount, currIndex + 1, newNeighborhoodCount, houses[currIndex]);
-        } else {
-            int totalColors = cost[0].length;
-            
-            // If the house is not painted, try every possible color and store the minimum cost
-            for (int color = 1; color <= totalColors; color++) {
-                int newNeighborhoodCount = neighborhoodCount + (color != prevHouseColor ? 1 : 0);
-                int currCost = cost[currIndex][color - 1] 
-                    + findMinCost(houses, cost, targetCount, currIndex + 1, newNeighborhoodCount, color);
-                minCost = Math.min(minCost, currCost);
-            }
-        }
-        
-        // Return the minimum cost and also storing it for future reference (memoization)
-        return memo[currIndex][neighborhoodCount][prevHouseColor] = minCost;
-    }
+    Map<String, Integer> map = new HashMap<>();
+    Integer memo[][][] = new Integer[101][21][101];
+    final int MAX = Integer.MAX_VALUE/2;
     
     public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
-        int answer = findMinCost(houses, cost, target, 0, 0, 0);
-        // Return -1 if the answer is MAX_COST as it implies no answer possible
-        return answer == MAX_COST ? -1 : answer;
+        int ans = getCost(0, 0, 0, houses, cost, m, n, target);
+        
+        return ans==Integer.MAX_VALUE/2?-1: ans;
     }
+    
+    private int getCost (int i, int l, int t, int[] houses, int[][] cost, int m, int n, int target){
+        if(t > target) return Integer.MAX_VALUE/2;
+        
+        if(i == m){
+            return t==target ? 0 : Integer.MAX_VALUE/2;
+        }
+        else if(memo[i][l][t] != null){
+            return memo[i][l][t];
+        }
+        // else if(map.containsKey(""+i+"*"+l+"*"+t)){
+        //     return map.get(""+i+"*"+l+"*"+t);
+        // }
+        
+        int ans = Integer.MAX_VALUE/2;
+        if(houses[i] == 0){
+            for(int j=0; j<n; j++){
+                ans = Math.min(ans,         
+                          cost[i][j]+getCost(i+1, j+1, (l==j+1)?t:t+1, houses, cost, m, n, target ));
+                }
+            }
+        else{
+            ans = Math.min(ans, 
+                    getCost(i+1, houses[i], (l==houses[i])?t:t+1, houses, cost, m, n,target));
+            }
+        
+            // map.put(""+i+"*"+l+"*"+t, ans);
+        memo[i][l][t] = ans;
+            return ans;
+        
+    }
+    
 }
