fd591b48c31b5766d9ab7d788b7334b3babd048b
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-08 16:29:51 +0530

    Time: 20 ms (16.59%), Space: 50.8 MB (52.71%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -3,11 +3,6 @@ class Solution {
         
         Arrays.sort(boxTypes, (a, b)-> b[1]-a[1]);
         
-        // for(int i=0; i<boxTypes.length; i++){
-        // System.out.print(boxTypes[i][0]+" "+boxTypes[i][1]);
-        // System.out.println("");
-        // }
-        
         int unit = 0;
         for(int box[] : boxTypes){
             
@@ -16,7 +11,7 @@ class Solution {
                 truckSize -= box[0];
             }else{
                 unit += (truckSize*box[1]);
-                truckSize -= truckSize;
+                return unit;
             }
         }
         
