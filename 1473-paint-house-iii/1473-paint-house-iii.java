a240a5849f336a19152d74e738e09b72eb0213cc
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-08 15:14:53 +0530

    Time: 66 ms (33.44%), Space: 73 MB (15.31%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.java
+++ 1473-paint-house-iii/1473-paint-house-iii.java
@@ -1,45 +1,36 @@
 class Solution {
-    // Map<String, Integer> map = new HashMap<>();
-    int memo[][][] = new int[101][21][101];
-    final int MAX = Integer.MAX_VALUE/2;
     
+   final int MAX = Integer.MAX_VALUE/2;
+    Integer memo[][][] = new Integer[101][21][101];
     
     public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
         int ans = getCost(0, 0, 0, houses, cost, m, n, target);
         
-        return ans==MAX?-1: ans;
+        return ans==MAX?-1:ans;
     }
     
-    
-    
-    private int getCost (int i, int l, int t, int[] houses, int[][] cost, int m, int n, int target){
-        if(t > target) return MAX;
-        
-        if(i == m){
-            return t==target ? 0 : MAX;
+    private int getCost(int i, int l, int t, int[] houses, int[][] cost, int m, int n, int target){
+        if(t > target){
+            return MAX;
         }
-        else if(memo[i][l][t] != 0){
+        if(i == m){
+            return t==target?0:MAX;
+        }
+        else if(memo[i][l][t] != null){
             return memo[i][l][t];
         }
-        // else if(map.containsKey(""+i+"*"+l+"*"+t)){
-        //     return map.get(""+i+"*"+l+"*"+t);
-        // }
         
         int ans = MAX;
         if(houses[i] == 0){
             for(int j=0; j<n; j++){
-                ans = Math.min(ans,         
-                      cost[i][j]+getCost(i+1, j+1, (l==j+1)?t:t+1, houses, cost, m, n, target ));
-                }
+              ans = Math.min(ans, cost[i][j]+getCost(i+1, j+1, (l==j+1?t:t+1), houses, cost, m, n, target ));  
             }
+        }
         else{
-            ans = Math.min(ans, 
-                  getCost(i+1, houses[i], (l==houses[i])?t:t+1, houses, cost, m, n,target));
-            }
+            ans = Math.min(ans, getCost(i+1, houses[i], (l==houses[i]?t:t+1), houses, cost, m, n, target )); 
+        }
         
-        // map.put(""+i+"*"+l+"*"+t, ans);
         memo[i][l][t] = ans;
         return ans;
     }
-    
 }
