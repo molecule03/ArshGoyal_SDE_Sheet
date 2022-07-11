d3e7f451f5a714334dafe52951c8e148692d5282
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-11 15:14:59 +0530

    Added solution - LeetHub

---
+++ Reaching the heights - GFG/reaching-the-heights.java
@@ -0,0 +1,92 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int N = Integer.parseInt(element[0]);
+		    int arr [] = new int[N];
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<N;i++){
+	            arr[i] = Integer.parseInt(elements[i]);    
+	        }
+		    
+		    
+		    Complete obj = new Complete();
+		    ArrayList<Integer> ans;
+		    ans = obj.reaching_height(N, arr);
+        	
+        	if(ans.size() == 1 && ans.get(0) == -1){
+        	    System.out.println("Not Possible");
+        	    continue;
+        	}
+        	
+        	for(int i: ans)
+        	    System.out.print(i + " ");
+        	System.out.println();
+		}
+	}
+}
+
+
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static ArrayList<Integer> reaching_height (int n, int arr[]) {
+        
+        int i = 0, j= n-1;
+        Arrays.sort(arr);
+        int height = 0;
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        for(int k=0; k<n; k++)
+        {
+            if(k%2==0)
+            {
+                ans.add(arr[j]);
+                height += arr[j--];
+            }
+            else
+            {
+                ans.add(arr[i]);
+                height -= arr[i++];
+            }
+        }
+        
+        if(height<=0) return new ArrayList<Integer>(Arrays.asList(-1));
+        
+        return ans;
+    }
+    
+    
+}
