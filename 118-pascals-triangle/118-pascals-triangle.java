9f4d9d507fb81fc357c0af66157913504ad943e1
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-19 13:15:17 +0530

    Time: 1 ms (72.34%), Space: 42.1 MB (29.96%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -3,13 +3,9 @@ class Solution {
         
         
         List<List<Integer>> wrapList = new ArrayList<>();
-        List<Integer> fl = new ArrayList<>();
-        fl.add(1);
-        wrapList.add(fl);
-        // wrapList.add(Arrays.asList(new int[]{1}));
-        for(int i=1; i<numRows; i++){
+        List<Integer> pre = null;
+        for(int i=0; i<numRows; i++){
             List<Integer> list = new ArrayList<>();
-            List<Integer> pre = wrapList.get(i-1);
             for(int j=0; j<=i; j++){
                 if(j == 0 || j==i){
                     list.add(1);
@@ -18,6 +14,7 @@ class Solution {
                     list.add(pre.get(j)+pre.get(j-1));
                 }
             }
+            pre = list;
             wrapList.add(list);
         }
         
