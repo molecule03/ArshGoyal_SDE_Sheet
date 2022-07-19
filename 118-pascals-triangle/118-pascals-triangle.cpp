8d3b45c68cafc8a4b6aa988831530a7d20a361e9
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-19 13:28:09 +0530

    Time: 3 ms (38.93%), Space: 6.6 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,23 +1,17 @@
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
+public:
+    vector<vector<int>> generate(int numRows) {
         
-        
-        List<List<Integer>> wrapList = new ArrayList<>();
-        ArrayList<Integer> pre = null;
-        for(int i=0; i<numRows; i++){
-            var list = new ArrayList<Integer>();
-            for(int j=0; j<=i; j++){
-                if(j == 0 || j==i){
-                    list.add(1);
-                }
-                else{
-                    list.add(pre.get(j)+pre.get(j-1));
-                }
-            }
-            pre = list;
-            wrapList.add(list);
+        vector<vector<int>> r(numRows);
+
+        for (int i = 0; i < numRows; i++) {
+            r[i].resize(i + 1);
+            r[i][0] = r[i][i] = 1;
+  
+            for (int j = 1; j < i; j++)
+                r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
         }
         
-        return wrapList;
+        return r;
     }
-}
+};
