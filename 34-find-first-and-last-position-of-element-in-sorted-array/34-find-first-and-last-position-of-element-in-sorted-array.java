c28808b4ec18cd1526a761c3d87d736c50a19dcd
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-25 11:13:16 +0530

    Time: 0 ms (100.00%), Space: 47.2 MB (49.67%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,49 +2,44 @@ class Solution {
     public int[] searchRange(int[] nums, int target) {
         if(nums.length == 0) return new int[]{-1, -1};
         
-        int first = 0;
-        int last = 0;
-        
+        int first = -1;
         int left = 0;
         int right = nums.length-1;
-        
         while(left <= right){
-             int mid = left + (right - left)/2;
+            int mid = left + (right - left)/2;
             
-          if(nums[mid] < target){
-                left = mid+1;
-          }
-            else if(nums[mid] >= target){
+            if(nums[mid] == target){
                 first = mid;
                 right = mid-1;
             }
-        }
-        // System.out.println(first);
-        
-        
-        left = 0;
-        right = nums.length-1;
-        last = -1;
-         while(left <= right){
-             int mid = left + (right - left)/2;
-            
-          if(nums[mid] <= target){
-                last = mid;
-              left = mid+1 ;
-          }
-            else if(nums[mid] > target){
+            else if(nums[mid] < target){
+                left = mid+1;
+            }
+            else{
                 right = mid-1;
             }
-             
         }
         
-        if(nums[first] == target && nums[last]==target){
-            return new int[]{first, last};
+        
+        int last = -1;
+        left = 0;
+        right = nums.length-1;
+        while(left <= right){
+           int mid = left + (right - left)/2;
+            
+            if(nums[mid] == target){
+                last = mid;
+                left = mid+1;
+            }
+            else if(nums[mid] < target){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }   
         }
-        // System.out.println(last);
+        
        
-        
-        
-        return new int[]{-1, -1};
+        return new int[]{first, last};
     }
 }
