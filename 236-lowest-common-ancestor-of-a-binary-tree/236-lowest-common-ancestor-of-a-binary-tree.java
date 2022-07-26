34c3ec2fad42222171d0c76b2d5188cc23574773
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-26 14:47:05 +0530

    Time: 11 ms (49.59%), Space: 47.9 MB (18.43%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -22,18 +22,13 @@ class Solution {
         if(root== null ) return null;
         
         if(root.val == q.val || root.val == p.val) return root;
-        // System.out.println(root.val+" "+target.val);
-        
+
        TreeNode left = getNode(root.left, p, q);
        TreeNode right = getNode(root.right, p, q);
         
-        // // return (left.val == target.val) ? left : right; 
-        // return left;
+        
         if(left != null && right != null) return root;
         
-        if(left == null)
-            return right;
-        else
-            return left;
+        return (left == null) ? right : left;
     }
 }
