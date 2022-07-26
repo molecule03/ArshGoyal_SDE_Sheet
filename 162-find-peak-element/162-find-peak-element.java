f9325824648289586b609c4e1e20736d2a9d6efb
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-26 11:45:33 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (51.68%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -8,7 +8,7 @@ class Solution {
         while(lo <= hi){
             int mid = lo + (hi-lo)/2;
             
-            if(mid-1 >= 0 && mid+1 < nums.length){
+            if(mid-1 >= 0 && mid+1 < nums.length){   // This is for Normal Cases
                 
                 if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
                     return mid;
@@ -20,7 +20,7 @@ class Solution {
                     hi = mid-1;
                 }
             }
-            else if(mid == 0){
+            else if(mid == 0){                        // This is to handle if mid is at 0 so their cant                                                           be mid-1 element
                 if(nums[mid] > nums[mid+1]){
                     return mid;
                 }
@@ -28,7 +28,7 @@ class Solution {
                     return mid+1;
                 }
             }
-            else if(mid == nums.length-1){
+            else if(mid == nums.length-1){           //his is to handle if mid is  last.. so their                                                        cant be mid+1 element
                 if(nums[mid] > nums[mid-1]){
                     return mid;
                 }
