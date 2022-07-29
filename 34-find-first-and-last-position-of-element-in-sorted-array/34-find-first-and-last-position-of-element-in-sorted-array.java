270383bb0fe3e66459fcd1e0a8ad8acd942c357e
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-29 23:28:43 +0530

    Time: 1 ms (38.49%), Space: 47.6 MB (15.36%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         
-        int first = -1;
         int left = 0;
         int right = nums.length-1;
+        int first = -1;
         while(left <= right){
             int mid = left + (right - left)/2;
             
@@ -19,12 +19,11 @@ class Solution {
             }
         }
         
-        
-        int last = -1;
         left = 0;
         right = nums.length-1;
+        int last = -1;
         while(left <= right){
-           int mid = left + (right - left)/2;
+            int mid = left + (right - left)/2;
             
             if(nums[mid] == target){
                 last = mid;
@@ -35,10 +34,55 @@ class Solution {
             }
             else{
                 right = mid-1;
-            }   
+            }
         }
-        
         
         return new int[]{first, last};
     }
 }
+
+
+
+
+
+
+//  10^9
+
+
+
+// 10^5 = 10^5
+// 10^4 =  10^8
+
+// 10^6 =  10^6*log(10^6)
+// 10^7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
