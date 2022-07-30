ef9a8e9050be40663ca6a3d6b4b8c4e0d67cd584
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-07-30 20:42:10 +0530

    Time: 115 ms (15.04%), Space: 119.4 MB (5.69%) - LeetHub

--- 916-word-subsets/916-word-subsets.java
+++ 916-word-subsets/916-word-subsets.java
@@ -1,28 +1,39 @@
 class Solution {
-    public List<String> wordSubsets(String[] A, String[] B) {
-        int[] bmax = count("");
-        for (String b: B) {
-            int[] bCount = count(b);
-            for (int i = 0; i < 26; ++i)
-                bmax[i] = Math.max(bmax[i], bCount[i]);
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        
+        int freq[] = new int[26];
+        Set<Character> wordsInB = new HashSet<>();
+        for(String b : words2){
+            int temp[] = count(b);
+            for(char c : b.toCharArray()){
+                wordsInB.add(c);
+                freq[c-'a'] = Math.max(freq[c-'a'], temp[c-'a']);
+            }
         }
-
-        List<String> ans = new ArrayList();
-        search: for (String a: A) {
-            int[] aCount = count(a);
-            for (int i = 0; i < 26; ++i)
-                if (aCount[i] < bmax[i])
+        
+        
+        List<String> res = new ArrayList<>();
+        search : for(String s : words1){
+            Map<Character, Integer> map = new HashMap<>();
+            for(char c : s.toCharArray()) map.put(c, map.getOrDefault(c, 0)+1);
+            
+            for(char a : wordsInB){
+                if(!map.containsKey(a) || map.get(a) < freq[a-'a'])
                     continue search;
-            ans.add(a);
+            }
+            
+            res.add(s);
         }
-
-        return ans;
+        
+        return res;
     }
-
-    public int[] count(String S) {
-        int[] ans = new int[26];
-        for (char c: S.toCharArray())
-            ans[c - 'a']++;
-        return ans;
+    
+    
+    private static int[] count(String b){
+        
+        int freq[] = new int[26];
+        for(char c : b.toCharArray()) freq[c-'a']++;
+        
+        return freq;
     }
 }
