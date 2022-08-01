830e752b27c6ccbcf34bb907a732092dda4c7f08
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-08-01 23:29:41 +0530

    Time: 3 ms (6.01%), Space: 43.8 MB (5.09%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,40 @@
+class Solution {
+    
+    int memo[][] = new int[101][101];
+    // int right = 0;
+    // int down = 0;
+    int m;
+    int n;
+    
+    public int uniquePaths(int m, int n) {
+        this.m = m;
+        this.n = n;
+        // if(right == m-1 && left == n-1) return 1;
+        
+        
+        int ans = paths(0, 0);
+        
+        return ans;
+        
+    }
+    
+    public int paths(int right, int down){
+        if(right == n-1 && down == m-1) return 1;
+        // System.out.println(right+" "+down);
+        if(memo[right][down] != 0) return memo[right][down];
+        
+        int ans=0;
+        if(right == n-1){
+            ans = paths(right, down+1);
+        }
+        else if(down == m-1){
+            ans = paths(right+1, down);
+        }
+        else{
+            ans = paths(right+1, down) + paths(right, down+1);
+        }
+        
+        memo[right][down] = ans;
+        return ans;
+    }
+}
