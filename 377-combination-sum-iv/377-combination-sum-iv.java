46e10865741801653bcfdf3caaa6e8ac2264271d
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-08-05 23:43:32 +0530

    Time: 2 ms (65.18%), Space: 41.3 MB (67.69%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,29 +1,16 @@
 class Solution {
    public int combinationSum4(int[] nums, int target) {
     // at start, none of the values are computed
-        int dp[] = new int[target+1];
+       int dp[] = new int[target+1];
        dp[0] = 1;
-    	for (int curTarget = 1; curTarget <= target; curTarget++) 
-        for (int num : nums)     // check for all elements from nums
-            if (num <= curTarget)  // an element will only be taken if it is less than target to be achieved
-                // the optimal answer(no. of combination) of remaining value is added to dp[curTarget]
-				dp[curTarget] += dp[curTarget - num];   
-    return dp[target];
+       for (int curTarget = 1; curTarget <= target; curTarget++){ 
+           for (int num : nums){    // check for all elements from nums
+               if (num <= curTarget)  // an element will only be taken if it is less than target to be achieved
+                   // the optimal answer(no. of combination) of remaining value is added to dp[curTarget]
+                   dp[curTarget] += dp[curTarget - num];  
+           }
+       }
+       
+       return dp[target];
     }
-    
-//     private int helper(int[] nums, int target, int[] dp){
-//         if(target == 0) return 1;  // base condition
-        
-//         if(dp[target] != 0) return dp[target];  // if already computed for this value
-        
-//         // recursive code starts -
-//         dp[target] = 0;
-        
-//         // check for every element of nums. An element can only be taken if it is less than target.
-//         // If an element is chosen, recurse for remaining value.
-//         for(int num : nums) 
-//             if(num <= target) dp[target] += helper(nums, target - num, dp);
-//         return dp[target];
-//     }
-    
 }
