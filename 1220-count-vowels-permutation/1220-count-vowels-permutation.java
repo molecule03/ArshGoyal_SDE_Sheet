5743bff0eec8336578fcbea903f3c5e73e0b64fa
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-08-08 00:06:51 +0530

    Time: 509 ms (5.07%), Space: 194 MB (5.07%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -0,0 +1,49 @@
+class Solution {
+    
+    int mod = 1000000007;
+    Map<String, Long> map = new HashMap<>();
+    public int countVowelPermutation(int n) {
+        
+        long total = 0;
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        for(char c : vowels){
+            total = (total + getString(n-1, c)) % mod;
+        }
+        
+        return (int)total;
+    }
+    
+    private long getString(int n, char curChar){
+        if(n == 0) return 1;
+        
+        String key = "" + n + curChar;
+        if(map.containsKey(key)) return map.get(key);
+        
+        long total = 0;
+        switch(curChar){
+            case 'a' :{
+                total = (getString(n-1, 'e'))%mod;
+                break;
+            }
+            case 'e':{
+                total = (getString(n-1, 'a') + getString(n-1, 'i'))%mod;
+                break;
+            }
+            case 'i':{
+                total = (getString(n-1, 'a')+getString(n-1, 'e')+getString(n-1, 'o')+getString(n-1, 'u'))%mod;
+                break;
+            }
+            case 'o':{
+                total = (getString(n-1, 'i')+ getString(n-1, 'u'))%mod;
+                break;
+            }
+            case 'u':{
+                total = (getString(n-1, 'a'))%mod;
+                break;
+            }
+        }
+        
+        map.put(key, total);
+        return total;
+    }
+}
