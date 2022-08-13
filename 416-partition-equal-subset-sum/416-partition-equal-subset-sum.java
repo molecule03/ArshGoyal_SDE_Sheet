4176dbac5755d9cf81fb72e659645287bc88eef7
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-08-14 00:40:18 +0530

    Time: 91 ms (34.48%), Space: 54.5 MB (44.06%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,34 +1,37 @@
 class Solution {
     
-    Boolean dp[][];
-    public boolean canPartition(int[] nums) {
+   	 public boolean canPartition(int[] nums) {
         
+        int n = nums.length;
         int sum = 0;
-        for(int i: nums) sum += i;
-        
-        if(sum%2 != 0) return false;
-        
-        dp = new Boolean[nums.length+1][sum+1];
-     
-        return getSum(nums, 0, sum, nums.length);
-    }
-    
-    private boolean getSum(int[] nums, int curSum, int totSum, int n){
-        if(n == 0 || totSum == 0) return false;
-        
-        if(curSum == totSum) return true;
-        
-        if(dp[n][totSum] != null) return dp[n][totSum];
-        
-        boolean ans = false;
-        if(nums[n-1] < totSum){
-            ans = getSum(nums, curSum+nums[n-1], totSum-nums[n-1], n-1) || getSum(nums, curSum, totSum, n-1);
-        }
-        else{
-            ans = getSum(nums, curSum, totSum, n-1);
+        for(int i : nums){
+            sum += i;
         }
         
-        dp[n][totSum] = ans;
-        return ans;
+         if(sum % 2 != 0) return false;
+         
+        boolean dp[][] = new boolean[n+1][(sum/2)+1];
+        
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<(sum/2)+1; j++){
+                if(i==0)
+                    dp[i][j] = false;
+                if(j == 0)
+                    dp[i][j] = true;
+            }
+        }
+        
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<(sum/2)+1; j++){
+                if(nums[i-1] <= j){
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][sum/2];
     }
 }
