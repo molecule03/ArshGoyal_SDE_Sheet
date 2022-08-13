a5de727945a5d45aa80c3905998a50ef2049f8e7
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-08-14 00:04:21 +0530

    Time: 142 ms (19.42%), Space: 70.8 MB (17.49%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -3,10 +3,9 @@ class Solution {
     Boolean dp[][];
     public boolean canPartition(int[] nums) {
         
-           int sum = 0;
-        for(int i: nums){
-            sum += i;
-        }
+        int sum = 0;
+        for(int i: nums) sum += i;
+        
         dp = new Boolean[nums.length+1][sum+1];
      
         return getSum(nums, 0, sum, nums.length);
@@ -15,20 +14,19 @@ class Solution {
     private boolean getSum(int[] nums, int curSum, int totSum, int n){
         if(n == 0 || totSum == 0) return false;
         
-        if(curSum*2 == totSum) return true;
+        if(curSum == totSum) return true;
         
-        if(dp[n][curSum] != null) return dp[n][curSum];
+        if(dp[n][totSum] != null) return dp[n][totSum];
         
         boolean ans = false;
         if(nums[n-1] < totSum){
-            ans = getSum(nums, curSum+nums[n-1], totSum, n-1) || getSum(nums, curSum, totSum, n-1);
+            ans = getSum(nums, curSum+nums[n-1], totSum-nums[n-1], n-1) || getSum(nums, curSum, totSum, n-1);
         }
         else{
             ans = getSum(nums, curSum, totSum, n-1);
         }
         
-        dp[n][curSum] = ans;
-        
+        dp[n][totSum] = ans;
         return ans;
     }
 }
