7dcdc079ded73192cfb6191a251e1ff2c7546211
Lavesh Salaskar <73989206+molecule03@users.noreply.github.com>
2022-08-19 23:56:17 +0530

    Time: 345 ms (30.67%), Space: 145.1 MB (28.47%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -2,17 +2,16 @@ class Solution {
     public int[] findOriginalArray(int[] changed) {
         if(changed.length%2 != 0) return new int[]{};
         
-        // List<Integer> list = new ArrayList<>();
-        // for(int i : changed) list.add(i);
         Map<Integer, Integer> map = new HashMap<>();
         for(int i: changed) map.put(i, map.getOrDefault(i, 0)+1);
         
+        if(map.containsKey(0) && map.get(0)%2 != 0) return new int[]{};
         
         Arrays.sort(changed);
         List<Integer> res = new ArrayList<>();
         for(int i : changed){
             if(map.get(i) == 0)continue;
-            if(i == 0 && map.get(i)%2 != 0) continue;
+            // if(i == 0 && map.get(i)%2 != 0) continue;
             if(map.containsKey(i*2)  && map.get(i*2) != 0){
                 res.add(i);
                 map.put(i, map.get(i)-1);
@@ -27,23 +26,6 @@ class Solution {
         for(int i: res){
             ans[j++] = i;
         }
-        // for(Integer i : list){
-        //     Integer a = i;
-        //     if(list.contains(a*2)){
-        //         res.add(a);
-        //         list.remove(a);
-        //         Integer l = a*2;
-        //         list.remove(l);
-        //     }
-        // }
-        
-        // return changed;
-        // if(res.size() == changed.length/2){
-        //     System.out.println(res);
-        // }
-        // else{
-        //     System.out.println("Isha");
-        // }
         
         return ans;
     }
